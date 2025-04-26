c36f5b80421d1fb4419cd92e453d5839768cc4dd
NEXTGEN <157381692+Nextgen112@users.noreply.github.com>
2025-04-26 23:02:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // ✅ IP Allowlist
-const allowedIps = ['62.201.240.35', '62.201.243.131', '127.0.0.1']; // Your allowed IPs here
+const allowedIps = ['62.201.240.35', '62.201.243.131', '62.201.240.35']; // Your allowed IPs here
 
 app.use((req, res, next) => {
   const requestIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
